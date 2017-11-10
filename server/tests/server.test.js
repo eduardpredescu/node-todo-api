8141d8e18af99f65d516fac5979bed4dab428afc
Eduard Predescu <eduard.florin.predescu@gmail.com>
2017-11-10 21:10:32 +0200

    added DELETE route tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -79,7 +79,6 @@ describe('GET /todos', () => {
 
 describe('GET /todos/:id', () => {
   it('should return todo doc', (done) => {
-    console.log(todos[0]._id.toHexString())
     request(app)
       .get(`/todos/${todos[0]._id.toHexString()}`)
       .expect(200)
@@ -101,5 +100,39 @@ describe('GET /todos/:id', () => {
       .get('/todos/123')
       .expect(404)
       .end(done);
-  })
-})
+  });
+});
+
+describe('DELETE /todos/:id', () => {
+  it('should delete and return todo', (done) => {
+    request(app)
+      .delete(`/todos/${todos[1]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(todos[1]._id.toHexString());
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+
+      Todo.findById(todos[1]._id.toHexString()).then((todo) => {
+        expect(todo).toNotExist();
+        done();
+      }).catch((e) => done(e));
+    });
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    request(app)
+      .delete(`/todos/${new ObjectID().toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for non-object ids', (done) => {
+    request(app)
+      .delete('/todos/123')
+      .expect(404)
+      .end(done);
+  });
+
+});
