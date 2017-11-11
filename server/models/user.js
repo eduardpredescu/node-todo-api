47fb692830b7fce53c033ff535a4f0ecd8b721ae
Eduard Predescu <eduard.florin.predescu@gmail.com>
2017-11-11 15:24:24 +0200

    added logout

--- server/models/user.js
+++ server/models/user.js
@@ -53,7 +53,17 @@ UserSchema.methods.generateAuthToken = function () {
   return user.save().then(() => {
     return token;
   });
-}
+};
+
+UserSchema.methods.removeToken = function (token) {
+  let user = this;
+  
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
 
 UserSchema.statics.findByToken = function (token) {
   let User = this;
